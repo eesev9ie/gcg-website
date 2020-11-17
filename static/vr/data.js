719211d7c4383ef0d9e09e5814e7f999e04ea011
Denys Konovalov <denys.konovalov@protonmail.com>
2020-11-17 19:57:27 +0100

    Schnittstelle für einen digitalen Schulrundgang hinzugefügt

---
+++ static/vr/data.js
@@ -0,0 +1,213 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-san-francisco",
+      "name": "San Francisco",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1571.5,
+      "initialViewParameters": {
+        "yaw": 1.628731098497962,
+        "pitch": 0.112272279609261,
+        "fov": 1.3900591270580378
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6728928501031755,
+          "pitch": -0.08252116146336874,
+          "rotation": 6.283185307179586,
+          "target": "2-schlafzimmer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.1959942749563464,
+          "pitch": 0.3502609665210201,
+          "title": "Lorem ipsum<br>",
+          "text": "<div><b>Lorem ipsum<b> dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.<img src='/media/gcg.JPG'></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-golden-gate-bridge",
+      "name": "Golden Gate Bridge",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1568.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3626509967165372,
+          "pitch": 0.1809987436227729,
+          "rotation": 0,
+          "target": "3-miami"
+        },
+        {
+          "yaw": 3.12673487759844,
+          "pitch": 0.3545497178692312,
+          "rotation": 0,
+          "target": "2-schlafzimmer"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-schlafzimmer",
+      "name": "Schlafzimmer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1718.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.36315495679741616,
+          "pitch": 0.157679890305376,
+          "rotation": 0,
+          "target": "1-golden-gate-bridge"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-miami",
+      "name": "Miami",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9039237094892787,
+          "pitch": 0.2814622722785156,
+          "rotation": 0,
+          "target": "4-panorama"
+        },
+        {
+          "yaw": 3.0436352502239483,
+          "pitch": 0.22521519934523582,
+          "rotation": 0,
+          "target": "0-san-francisco"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-panorama",
+      "name": "Panorama",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Georg-Cantor-Gymnasium: Virtueller Tag der offenen Tür",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
