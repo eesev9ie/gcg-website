6bdb1eb2433a85d5806e7b456be3736bcf3fb4cb
Denys Konovalov <denys.konovalov@protonmail.com>
2020-11-18 20:48:45 +0100

    - Aussehen der VR-Schnittstelle verändert - Schriftart-Quelle hinzugefügt

--- static/de/vr/data.js
+++ static/de/vr/data.js
@@ -41,7 +41,7 @@ var APP_DATA = {
           "yaw": 2.1959942749563464,
           "pitch": 0.3502609665210201,
           "title": "Lorem ipsum<br>",
-          "text": "<div><b>Lorem ipsum<b> dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.<img src='/media/gcg.JPG'></div>"
+          "text": "<div><h1>Lorem ipsum</h1><br> dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.<img src='/media/gcg.JPG'></div>"
         }
       ]
     },
